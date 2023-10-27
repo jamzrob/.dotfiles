fab13524980f04dbf16af55811bce4efbbd65dd4
James Raubenheimer <raubenheimerjames@gmail.com>
2023-10-27 13:54:57 -0400

    Updated

--- .config/nvim/after/plugin/fugitive.lua
+++ .config/nvim/after/plugin/fugitive.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<leader>gc", vim.cmd.Git)
 vim.keymap.set("n", "<leader>gb", ':Git blame<CR>')
-vim.keymap.set("n", "<leader>gp", ':Git cip \"Updated\"')
+vim.keymap.set("n", "<leader>gp", ':Git cip \"Updated\"<CR>')
+vim.keymap.set("n", "<leader>gr", ':Git cip \"Updated\"<CR>')
 
